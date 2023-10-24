195edf3e821c11a619759bead9befb0848c2621a
Manoush Pajouh <mpajouh@acl205.cs.vassar.edu>
2023-10-24 17:09:21 -0400

    commented how to make rating

--- SearchLocationsPrototype/src/UI.java
+++ SearchLocationsPrototype/src/UI.java
@@ -20,43 +20,43 @@ public class UI {
        System.out.println("Please input desired location.");
        String searchLoc = s.nextLine();
 
-<<<<<<< HEAD
        SearchName sn = new SearchName();
        System.out.println(sn.searchName(searchLoc, c.locations));
+       if (c.searchByName(searchLoc) == null){ // if user input does not match any locations, say so
+           System.out.println("No locations match your search.");
+       }
+       else {
+           System.out.println(c.searchByName(searchLoc)); // otherwise, use searchByName method in controller to produce the correct location
+       }
+   }
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-       //need it to be in a loop so that if yes or no is put in wrong you can try again
+//need it to be in a loop so that if yes or no is put in wrong you can try again
        /*
            System.out.println("Want to add rating? Input 'Yes' or 'No'");
            String rateOption = s.nextLine();
             if (rateOption == "Yes"){
+                System.out.println("Input your desired rating number:");
+                String rateNum = s.nextLine();
                 System.out.println("Do you want to add a comment?");
                 String commentOption = s.nextLine();
                     if (commentOption == "Yes"){
-                        Rating rating =
+                        System.out.println("Please enter your comment:");
+                        String cm = s.nextLine();
+                        Rating rating = new Rating(rateNum, cm);
                     }
-
-
-
-                Rating rating = new Rating();
-                System.out.println("Add Your Rating of" + rating.number + "to" sn.)
+                    //dont want comment
+                    else (){
+                    Rating rating = new Rating(rateNum);
+                    System.out.println("Adding Your Rating of" + rating.number +"to" + c.searchByName(searchLoc))
+                    }
             }
+            //if they do not want to add a rating --> break out of loop
             else if (rateOption == "No"){
-
+                break;
             }
+            //if input doesnt match yes or no --> restart loop
             else {
 
             }
@@ -64,14 +64,4 @@ public class UI {
 
         */
 
-=======
-       if (c.searchByName(searchLoc) == null){ // if user input does not match any locations, say so
-           System.out.println("No locations match your search.");
-       }
-       else {
-           System.out.println(c.searchByName(searchLoc)); // otherwise, use searchByName method in controller to produce the correct location
-       }
-    }
->>>>>>> 34407989379ed072fae4573f3892c1b6b8ac4b54
-}
 
