d0d03237ecb9f27d6bf6f6bae79ad31ee5ee40ba
Manoush Pajouh <mpajouh@acl212.cs.vassar.edu>
2023-10-31 17:02:49 -0400

    implemented rating averages and adding ratings to location,rating,ui.java

--- SearchLocationsPrototype/src/UI.java
+++ SearchLocationsPrototype/src/UI.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
 /**
  * The user interface of this application: interacts with the Controller class to display all available locations to
  * the user and then displays the results of the user's search for a particular location.
@@ -21,47 +24,44 @@ public class UI {
        String searchLoc = s.nextLine();
 
        SearchName sn = new SearchName();
-       System.out.println(sn.search(searchLoc, c.locations));
+       Location foundLoc =  sn.search(searchLoc, c.locations); //specify the location found if it is already added
+       System.out.println(foundLoc);
        if (c.searchByName(searchLoc) == null){ // if user input does not match any locations, say so
            System.out.println("No locations match your search.");
        }
        else {
            System.out.println(c.searchByName(searchLoc)); // otherwise, use searchByName method in controller to produce the correct location
        }
+
+       //adding a new rating through user input terminal
+       System.out.println("Want to add rating? Input 'Yes' or 'No'");
+       String rateOption = s.nextLine();
+       if (rateOption.equals("Yes")){ //do not use == bc of memory issues
+           System.out.println("Input your desired rating number:");
+           int rateNum = parseInt(s.nextLine());            //convert rateNum from string to integer
+           System.out.println("Do you want to add a comment?");
+           String commentOption = s.nextLine();
+           if (commentOption.equals("Yes")){
+               System.out.println("Please enter your comment:");
+               String cm = s.nextLine();
+               Rating rating = new Rating(rateNum, cm);
+               //add rating to the list so that the counter will still work
+               foundLoc.allRatings.add(rating);
+               System.out.println("Adding Your Rating of " + rating.number +" to " + searchLoc);
+               System.out.println("New Overall Rating of " + foundLoc.crowdRating + "at " + searchLoc);
+           }
+           //dont want comment
+           else {
+               Rating rating = new Rating(rateNum);
+               //add rating to the list so that the counter will still work
+               foundLoc.allRatings.add(rating);
+               System.out.println("Adding Your Rating of " + rating.number +" to " + searchLoc);
+               System.out.println("New Overall Rating of " + foundLoc.getRatingAve() + " to " + searchLoc);
+           }
+       }
+
+
+
+
    }
 }
-
-
-//need it to be in a loop so that if yes or no is put in wrong you can try again
-       /*
-           System.out.println("Want to add rating? Input 'Yes' or 'No'");
-           String rateOption = s.nextLine();
-            if (rateOption == "Yes"){
-                System.out.println("Input your desired rating number:");
-                String rateNum = s.nextLine();
-                System.out.println("Do you want to add a comment?");
-                String commentOption = s.nextLine();
-                    if (commentOption == "Yes"){
-                        System.out.println("Please enter your comment:");
-                        String cm = s.nextLine();
-                        Rating rating = new Rating(rateNum, cm);
-                    }
-                    //dont want comment
-                    else (){
-                    Rating rating = new Rating(rateNum);
-                    System.out.println("Adding Your Rating of" + rating.number +"to" + c.searchByName(searchLoc))
-                    }
-            }
-            //if they do not want to add a rating --> break out of loop
-            else if (rateOption == "No"){
-                break;
-            }
-            //if input doesnt match yes or no --> restart loop
-            else {
-
-            }
-   }
-
-        */
-
-
