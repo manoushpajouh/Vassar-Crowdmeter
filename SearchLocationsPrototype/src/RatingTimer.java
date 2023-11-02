f2f84de1bbf4f10c7a0f22b90a8506d0b79899b2
Manoush Pajouh <mpajouh@ghopper.cs.vassar.edu>
2023-11-02 16:14:11 -0400

    made Rating Timer class

--- SearchLocationsPrototype/src/RatingTimer.java
+++ SearchLocationsPrototype/src/RatingTimer.java
@@ -1,2 +1,24 @@
 public class RatingTimer {
+
+
+    /**
+     * Constructor for the RatingTimer
+     *
+     */
+    public RatingTimer(){
+
+
+    }
+
+    /**
+     * The rating timer will reset every time a rating has been made
+     */
+
+
+
+
+
+
+
+
 }
