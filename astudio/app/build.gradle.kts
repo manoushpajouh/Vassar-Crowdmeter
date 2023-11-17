7895ba25ebb82568d382ed69894ea0cf0ae79dfb
Zachary Watson <zwatson@jgosling.cs.vassar.edu>
2023-11-17 16:22:05 -0500

    Adding search locations and add locations

--- astudio/app/build.gradle.kts
+++ astudio/app/build.gradle.kts
@@ -3,12 +3,12 @@ plugins {
 }
 
 android {
-    namespace = "com.example.vassarcrowdmeterapp"
-    compileSdk = 33
+    namespace = "com.example.crowdmeterproject"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.vassarcrowdmeterapp"
-        minSdk = 24
+        applicationId = "com.example.crowdmeterproject"
+        minSdk = 30
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -26,12 +26,15 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures{
+        viewBinding = true
+}
 }
 
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
